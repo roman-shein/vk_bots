e3377fc8080982cfc084ac52daa12c019c1fc7d8
Yandex <roman.shein.07@gmail.com>
2025-05-16 17:19:29 +0300

    2.2

--- Запрос Сортированные друзья.py
+++ Запрос Сортированные друзья.py
@@ -1,10 +1,49 @@
 import vk_api
-from config import LOGIN, PASSWORD, TOKEN
+from config import LOGIN, PASSWORD
+from datetime import datetime
+
+
+def captcha_handler(captcha):
+    """ При возникновении капчи вызывается эта функция и ей передается объект
+        капчи. Через метод get_url можно получить ссылку на изображение.
+        Через метод try_again можно попытаться отправить запрос с кодом капчи
+    """
+
+    key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
+
+    # Пробуем снова отправить запрос с капчей
+    return captcha.try_again(key)
+
+
+def auth_handler():
+    """ При двухфакторной аутентификации вызывается эта функция. """
+
+    # Код двухфакторной аутентификации,
+    # который присылается по смс или уведомлением в мобильное приложение
+    # или код из приложения - генератора кодов
+    key = input("Enter authentication code: ")
+    # Если: True - сохранить, False - не сохранять.
+    remember_device = True
+
+    return key, remember_device
 
 
 def main():
-    vk_session = vk_api.VkApi(token=TOKEN)
+    login, password = LOGIN, PASSWORD
+    vk_session = vk_api.VkApi(
+        login, password,
+        # функция для обработки двухфакторной аутентификации
+        auth_handler=auth_handler,
+        captcha_handler=captcha_handler
+    )
+
+    try:
+        vk_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
     vk = vk_session.get_api()
+    # Используем метод wall.get
     arr = []
     response = vk.friends.get(fields="city,bdate")
     # print(response, file=open("data.json", 'w', encoding="utf8"))
@@ -28,7 +67,5 @@ def main():
         print(*el)
 
 
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
