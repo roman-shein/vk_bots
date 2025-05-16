c3db889cdc036400ed03d89e8d2ece5516392067
Yandex <roman.shein.07@gmail.com>
2025-05-16 17:17:17 +0300

    1.2

--- Запрос записи на стене.py
+++ Запрос записи на стене.py
@@ -1,21 +1,50 @@
 import vk_api
-from config import LOGIN, PASSWORD, TOKEN
+from config import LOGIN, PASSWORD
 from datetime import datetime
 
 
-def main():
-    # login, password = LOGIN, PASSWORD
-    vk_session = vk_api.VkApi(token=TOKEN)
-    vk = vk_session.get_api()
-    # try:
-    #     vk_session.auth(token_only=True)
-    # except vk_api.AuthError as error_msg:
-    #     print(1, error_msg)
-    #     return
+def captcha_handler(captcha):
+    """ При возникновении капчи вызывается эта функция и ей передается объект
+        капчи. Через метод get_url можно получить ссылку на изображение.
+        Через метод try_again можно попытаться отправить запрос с кодом капчи
+    """
 
+    key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
+
+    # Пробуем снова отправить запрос с капчей
+    return captcha.try_again(key)
+
+
+def auth_handler():
+    """ При двухфакторной аутентификации вызывается эта функция. """
+
+    # Код двухфакторной аутентификации,
+    # который присылается по смс или уведомлением в мобильное приложение
+    # или код из приложения - генератора кодов
+    key = input("Enter authentication code: ")
+    # Если: True - сохранить, False - не сохранять.
+    remember_device = True
+
+    return key, remember_device
+
+
+def main():
+    login, password = LOGIN, PASSWORD
+    vk_session = vk_api.VkApi(
+        login, password,
+        # функция для обработки двухфакторной аутентификации
+        auth_handler=auth_handler,
+        captcha_handler=captcha_handler
+    )
+
+    try:
+        vk_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+    vk = vk_session.get_api()
     # Используем метод wall.get
     response = vk.wall.get(count=5, offset=1)
-    # print(response, file=open("data.json", 'w', encoding="utf8"))
     if response['items']:
         for elem in response['items']:
             print(f'{elem["text"].rstrip()};')
